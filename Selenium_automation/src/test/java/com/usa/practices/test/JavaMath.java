f17aecba6e34e302a5cf0d92809192c9e214705e
Automation-Champion <59101916+Automation-Champion@users.noreply.github.com>
2019-12-26 11:00:25 -0500

    Update JavaMath.java

--- Selenium_automation/src/test/java/com/usa/practices/test/JavaMath.java
+++ Selenium_automation/src/test/java/com/usa/practices/test/JavaMath.java
@@ -13,7 +13,25 @@ public class JavaMath {
 					System.out.println(i);	
 				});
 				
-		
+				// This is a int data
+		int a = 1234;
+		String str = Integer.toString(a);
+		System.out.println("I am convert int to String  = " + str);
+
+		// This is a String data
+		String number = "10";
+		int result = Integer.parseInt(number);
+		System.out.println("I am convert String to int  = " + result);
+
+		String string1 = new String("smarttech");
+		string1 = "Alam";
+		//String string2 = new String("Practice");
+	
+
+		// Comparing for String 1 != String 2
+		//System.out.println("Comparing " + string1 + " and " + string2 + " : " + string1.equals(string2));
+		System.out.println("Comparing " + string1 + " and " + string1 + " : " + string1.equals(string1));
+
 
 	}
 
