161359a389d672709d0d32f241d74236e7f24a07
Automation-Champion <59101916+Automation-Champion@users.noreply.github.com>
2019-12-30 15:49:50 -0500

    Update AlamBhaiClass.java

--- Selenium_automation/src/test/java/com/usa/practices/test/AlamBhaiClass.java
+++ Selenium_automation/src/test/java/com/usa/practices/test/AlamBhaiClass.java
@@ -125,6 +125,18 @@ public class AlamBhaiClass {
 	        System.out.println(set.last());
 		
 	}
+	
+	
+	public class LargestNumberSample {
+	   public static void main(String args[]){
+	      int array[] = {10, 20, 25, 63, 96, 57};
+	      int size = array.length;
+	      Arrays.sort(array);
+	      System.out.println("sorted Array ::"+Arrays.toString(array));
+	      int res = array[size-2];
+	      System.out.println("2nd largest element is ::" + res);
+	   }
+	}
 }
 	 */		
 			
