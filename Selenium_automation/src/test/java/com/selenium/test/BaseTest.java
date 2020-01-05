a80eb2a33be19d0730b53da2b33f932964500f1d
Automation-Champion <weloveusa4ever@gmail.com>
2020-01-05 12:00:16 -0500

    update class

--- Selenium_automation/src/test/java/com/selenium/test/BaseTest.java
+++ Selenium_automation/src/test/java/com/selenium/test/BaseTest.java
@@ -40,3 +40,69 @@ public class BaseTest {
 		}
 		 
 		}
+
+//Types of X-path
+//There are two types of XPath:
+
+//1) Absolute XPath
+
+//2) Relative XPath
+
+//Absolute XPath:
+//It is the direct way to find the element, but the disadvantage of the absolute XPath is that if there are any changes made in the path of the element then that XPath gets failed.
+
+//The key characteristic of XPath is that it begins with the single forward slash(/) ,which means you can select the element from the root node.
+
+//Below is the example of an absolute xpath expression of the element shown in the below screen.
+
+//Absolute xpath:
+
+// html/body/div[1]/section/div[1]/div/div/div/div[1]/div/div/div/div/div[3]/div[1]/div/h4[1]/b
+
+//Relative xpath: //*[@class='featured-box']
+
+           // How to create xpath formula 
+        //Xpath=//tagname[@attribute='value']
+        // Xpath=//input[@name='uid']
+
+// text xpath method 
+          //*[text()='Testing']
+
+// contain method
+//Xpath=//*[contains(@type,'sub')] 
+
+//Xpath=//*[contains(text(),'here')]
+
+//Xpath=//*[@type='submit' or @name='btnReset']
+
+//Xpath=//label[starts-with(@id,'message')]
+//Xpath=//*[@type='text']//following::input
+//Xpath=//*[@type='submit']//preceding::input
+//xpath=//*[@type='submit']//following-sibling::input
+
+//Xpath=//*[@id='rt-feature']//parent::div
+
+//Xpath =//*[@type='password']//self::input
+//Xpath=//*[@id='rt-feature']//descendant::a
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
