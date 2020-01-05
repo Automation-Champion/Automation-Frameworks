428b321e733fd084847348b4a6f9729b5385e6d9
Automation-Champion <weloveusa4ever@gmail.com>
2020-01-05 11:47:47 -0500

    Update Selenium class

--- Selenium_automation/src/test/java/com/selenium/test/BaseTest.java
+++ Selenium_automation/src/test/java/com/selenium/test/BaseTest.java
@@ -3,24 +3,39 @@ package com.selenium.test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class BaseTest {
 	
 	
 	public static void main(String[] args) {
 		
+		// This is only fire-fox browser for selenium version 2 and fire-fox version 46.1
+		//WebDriver driver=new FirefoxDriver();  // This line call up casting 
+		
 		System.setProperty("webdriver.chrome.driver", "./Driver/chromedriver.exe");
-		 
+		
 		// Initialize browser
-		WebDriver driver=new ChromeDriver();
-		 
-		// Open face book
+	    WebDriver driver=new ChromeDriver();
+	 		 
+		// Open ups
 		driver.get("https://www.ups.com/us/en/Home.page");
 		 
 		// Maximize browser		 
 		driver.manage().window().maximize();
 		
-		driver.findElement(By.xpath("(//*[contains(text(), 'Log In')] )[1]")).click();
+		// How to implement id 
+		//driver.findElement(By.id("ups-menuLinks0")).click();
+		
+		driver.findElement(By.xpath("(//*[contains(text(),'Log In')])[1]")).click();
+		
+		driver.findElement(By.id("email")).sendKeys("alammohammed79@gmail.com");
+		
+		driver.findElement(By.id("pwd")).sendKeys("SAYEDawan2008");
+		
+		driver.findElement(By.id("submitBtn")).click();
+		
+		System.out.println(driver.getTitle());
 		 
 		}
 		 
